ecf99a9d88ca3903fa9ba7a331bf736736cabed4
aakaash029 <Aakash.24SCSE1180383@galgotiasuniversity.ac.in>
2025-08-31 11:04:23 +0530

    added

---
+++ aug27.java
@@ -0,0 +1,20 @@
+public class aug27 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        // before swapping 
+        System.out.println(a);
+        System.out.println(b);
+        // int temp;
+        // temp=a;
+        // a=b;
+        // b=temp;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        // after swapping
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
---
+++ aug30.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class aug30 {
+     public static void main(String[] args) {
+    //     //  int a=3567;
+         Scanner s=new Scanner(System.in);
+         System.out.println("Enter number:");
+         int a=s.nextInt();
+
+
+
+         
+
+    //     int count=0;          //counting the digit
+    //     while(a>0){
+    //         a=a/10;
+    //         count++;
+
+    //     }
+    //      s.close();
+    //     System.out.println(count);
+
+
+
+
+
+
+    int rev=0;               //reverse the digit
+    while(a>0){
+        int rem=a%10;
+         rev=rem+(rev*10);
+         a=a/10;
+    }
+    s.close();
+    System.out.println(rev);
+     }
+}
