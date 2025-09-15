5554bf3806bd6a6845a50d524a20d9efd61d38fb
aakaash029 <Aakash.24SCSE1180383@galgotiasuniversity.ac.in>
2025-09-15 22:11:53 +0530

    2

--- unit 2(Recursion)/pre.java
+++
@@ -1,7 +0,0 @@
-public class pre {
-    public static void main(String[] args) {
-        String arr[]={"apple","banana","orange"};
-        System.out.println(arr[0]);
-        System.out.println(arr.toString());
-    }
-}
